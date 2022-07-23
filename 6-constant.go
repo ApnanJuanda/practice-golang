b78d0303c98bd17faf7825d8a9bb70a475990e8f
Apnan Juanda <apnanjuanda@gmail.com>
2022-07-23 10:33:01 +0700

    mengubah code file 6

--- 6-constant.go
+++ 6-constant.go
@@ -3,12 +3,10 @@ package main
 import "fmt"
 
 func main(){
-	const firstName = "Apnan"
-	const lastName = "Juanda"
 
 	//Multiple initiate constants
 	const(
 		firstName = "Apnan"
-		
+		lastName = "Juanda"
 	)
 }
